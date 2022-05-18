4d2ff8f18482b9b76bcf717d4f7dc963c01862b4
seenusneha4 <seenaannmathew6@gmail.com>
2022-05-18 10:53:57 +0530

    navfst

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Viewusers from './Components/Viewusers';
+import Header from './Components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Viewusers/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Viewusers/>}/>
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
